124a749a993e990a56d676286a146fbab26b7e5d
songinsup <insup74@gmail.com>
2022-10-10 21:18:03 +0900

    First

--- display/src/main/java/com/elevenst/api/DisplayController.java
+++ display/src/main/java/com/elevenst/api/DisplayController.java
@@ -1,20 +1,23 @@
 package com.elevenst.api;
 
+import com.elevenst.service.FeignProductRemoteService;
 import com.elevenst.service.ProductRemoteService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
 @RequestMapping(path = "/displays")
 public class DisplayController {
 
     private final ProductRemoteService productRemoteService;
+    private final FeignProductRemoteService feignProductRemoteService;
 
-    public DisplayController(ProductRemoteService productRemoteService) {
+    public DisplayController(ProductRemoteService productRemoteService,
+                             FeignProductRemoteService feignProductRemoteService) {
         this.productRemoteService = productRemoteService;
+        this.feignProductRemoteService = feignProductRemoteService;
     }
 
     @GetMapping(path = "/{displayId}")
@@ -24,6 +27,6 @@ public class DisplayController {
     }
 
     private String getProductInfo() {
-        return productRemoteService.getProductInfo("12345");
+        return feignProductRemoteService.getProductInfo("12345");
     }
 }
--- product/src/main/java/com/elevenst/api/ProductController.java
+++ product/src/main/java/com/elevenst/api/ProductController.java
@@ -11,18 +11,13 @@ public class ProductController {
 
     @GetMapping(path = "{productId}")
     public String getProductInfo(@PathVariable String productId) {
-
-//        int milliseconds = 4000;
 //        try {
-//            Thread.sleep(milliseconds);
+//            Thread.sleep(2000);
 //        } catch (InterruptedException e) {
 //            e.printStackTrace();
 //        }
-//        System.out.println("after sleep "+(milliseconds/1000)+" seconds.");
 
-        String resultStr ="[product id = " + productId + " at " + System.currentTimeMillis() + "]";
-        System.out.println(resultStr);
-        return resultStr;
+        return "[product id = " + productId + " at " + System.currentTimeMillis() + "]";
 //        throw new RuntimeException("I/O Exception");
     }
 }
